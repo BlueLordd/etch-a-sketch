dda6eb837599a8e2d1cae0bbfe708cbb6c70a9ff
Nilkanta Roy <roynilkanta2000@gmail.com>
2023-10-11 19:42:53 +0530

    Create dynamic grid using user input via prompt

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 let container= document.querySelector(".container");
+const containerWidth= "500px";
+const containerHeight= "500px";
 
 
-function createGrid(){
-    makeColumns(16);
-    makeRows(16);
+// TAKING USER INPUT AND MAKING GRID OF INPUT SIZE
+let intInput;
+do{
+    let userInput= prompt("Enter Grid dimensions between 2 to 100 : ");
+    intInput= parseInt(userInput);
+    if( !isNaN(intInput) && intInput>=2 && intInput<=100)
+        break;
+}
+while(true)
+
+createGrid(intInput);
+
+function createGrid(input){
+    makeColumns(input);
+    makeRows(input);
 }
 
 function makeColumns(numCols){ //make all the column heads (i.e.-> 0th row)
     for(let i=0;i<numCols;i++){
         let col= document.createElement("div");
         col.className= "col";
+        col.style.width= `calc(${containerWidth} / ${numCols})`;
         container.appendChild(col);
     }
 }
+
 function makeRows(numRows){ //attach cells to each column head
     let cols= document.querySelectorAll(".col");
     for(let i=0;i<cols.length;i++){
         for(let j=0;j<numRows;j++){
             let cell= document.createElement("div");
             cell.className= "cell";
-            if(j===0){  //handle super-imposition of row and cell border for 0th row
-                cols[i].style.border= "none";
-            }
+            cell.style.width= `calc(${containerWidth} / ${numRows})`;
+            cell.style.height= `calc(${containerHeight} / ${numRows})`;
             cols[i].appendChild(cell);
         }
     }
 }
 
-createGrid();
 
-
-
-
+// GENERATE COLORS ON MOUSE HOVER BY USER
 let gridCell= document.querySelectorAll(".cell");
 
 gridCell.forEach( cell => {
