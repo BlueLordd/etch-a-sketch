454ea6566dc28acaa3046c35ae3fb40050bc5c78
Nilkanta Roy <roynilkanta2000@gmail.com>
2023-10-12 21:23:02 +0530

    Add user interface and CSS

--- script.js
+++ script.js
@@ -1,24 +1,77 @@
 let container= document.querySelector(".container");
-const containerWidth= "500px";
+const containerWidth= "600px";
 const containerHeight= "500px";
+let userInput=64;
+let cellColor;
 
 
-// TAKING USER INPUT AND MAKING GRID OF INPUT SIZE
-let intInput;
-do{
-    let userInput= prompt("Enter Grid dimensions between 2 to 100 : ");
-    intInput= parseInt(userInput);
-    if( !isNaN(intInput) && intInput>=2 && intInput<=100)
-        break;
-}
-while(true)
 
-createGrid(intInput);
+//dom needs to load first before taking user input via slider & furthur actions
+document.addEventListener("DOMContentLoaded", function() {
+    let slider = document.querySelector("#slider");
+    let currentSliderValue = document.querySelector("#sliderValue");
 
+    createGrid(userInput); //default initial values
+    cellColor= "black";
+
+    slider.addEventListener("input", function () {
+        userInput = this.value;
+        currentSliderValue.innerText = `${this.value}`;
+        createGrid(userInput);
+    });
+
+    const blackButton= document.querySelector("#black");
+    const rgbButton= document.querySelector("#rgb");
+    const eraseButton= document.querySelector("#erase");
+    blackButton.addEventListener("click", () => {
+        cellColor= "black";
+    })
+    rgbButton.addEventListener("click", () => {
+        cellColor= "rgb";
+    })  
+    eraseButton.addEventListener("click", () => {
+        cellColor= "none";
+    })
+
+    const colorPicker= document.querySelector("#colorPicker");
+    colorPicker.addEventListener("input", function(){
+        cellColor= this.value;
+    })
+});
+
+
+
+//create dynamic grid according to user input
 function createGrid(input){
-    makeColumns(input);
-    makeRows(input);
+    container.innerHTML="";
+    let numCols= input;
+    let numRows= input;
+    makeColumns(numCols);
+    makeRows(numRows);  
+
+    let gridCell= document.querySelectorAll(".cell");
+    gridCell.forEach( cell => {
+        cell.addEventListener("mouseenter", () => {
+            if(cellColor==="black")
+                cell.style.backgroundColor= "black";
+            else if(cellColor==="rgb")
+                cell.style.backgroundColor= rgbColor();
+            else if(cellColor==="none")
+                cell.style.backgroundColor= "transparent";
+            else
+                cell.style.backgroundColor= cellColor;
+        })
+    })
+
+    const resetButton= document.querySelector("#reset");
+    resetButton.addEventListener("click", () => {
+        gridCell.forEach( cell => {
+            cell.style.backgroundColor= "transparent";
+        })
+    })
 }
+
+
 
 function makeColumns(numCols){ //make all the column heads (i.e.-> 0th row)
     for(let i=0;i<numCols;i++){
@@ -28,7 +81,6 @@ function makeColumns(numCols){ //make all the column heads (i.e.-> 0th row)
         container.appendChild(col);
     }
 }
-
 function makeRows(numRows){ //attach cells to each column head
     let cols= document.querySelectorAll(".col");
     for(let i=0;i<cols.length;i++){
@@ -43,14 +95,6 @@ function makeRows(numRows){ //attach cells to each column head
 }
 
 
-// GENERATE COLORS ON MOUSE HOVER BY USER
-let gridCell= document.querySelectorAll(".cell");
-
-gridCell.forEach( cell => {
-    cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor= rgbColor();
-    })
-})
 
 function rgbColor(){
     const letters= "0123456789ABCDEF";
